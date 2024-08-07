3599c4e74ee025a3b08589e62ba3b9e3510a30f1
sharjeelhtconnects <sharjeel@htconnects.com>
2024-08-07 22:50:12 +0500

    ss

--- hatch/controllers/main.py
+++ hatch/controllers/main.py
@@ -13,39 +13,8 @@ class RecuritmentControllers(http.Controller):
     def get_opening(self):
         open_jobs = request.env['hr.job'].sudo().search(['|', ('state', '=', 'recruit'), ('website_published', '=', True)])
         job_data = {open_jobs}
+        host = socket.gethostname()
         try:
-            # job_data['jobs'] = open_jobs
-            # Convert data dictionary to JSON string
-            # json_data = json.dumps(job_data)
-            
-            # Convert the recordset to a list of dictionaries
-            job_data = []
-            for job in open_jobs:
-                job_data.append({
-                    'id': job.id,
-                    'name': job.name,
-                    'description': job.description,
-                    'department_id': job.department_id.name,
-                    # Add more fields as needed
-                })
-
-            # Convert list to JSON string
-            json_data = json.dumps(job_data)
-            return request.make_response(data=json_data, headers=[('Content-Type', 'application/json')])
-        except Exception as e:
-            se = _serialize_exception(e)
-            error = {
-                'code': 200,
-                'message': 'Something went wroung',
-                'data': se
-            }
-            return request.make_response(html_escape(json.dumps(error))) 
-    
-    @http.route('/job_form/<int:job_id>', type='http', auth='none')
-    def job_form(self, job_id):
-        open_jobs = request.env['hr.job'].sudo().search([('id', '=', job_id)])
-        try:
-            host = socket.gethostname()
             job_data = []
             for job in open_jobs:
                 title = (job.name).lower().replace(" ", "-")
@@ -69,4 +38,34 @@ class RecuritmentControllers(http.Controller):
                 'message': 'Something went wroung',
                 'data': se
             }
-            return request.make_response(html_escape(json.dumps(error)))
+            return request.make_response(html_escape(json.dumps(error))) 
+    
+    # @http.route('/job_form/<int:job_id>', type='http', auth='none')
+    # def job_form(self, job_id):
+    #     open_jobs = request.env['hr.job'].sudo().search([('id', '=', job_id)])
+    #     try:
+    #         host = socket.gethostname()
+    #         job_data = []
+    #         for job in open_jobs:
+    #             title = (job.name).lower().replace(" ", "-")
+    #             url = (str(host))+"/jobs/detail/"+title+"-"+(str(job.id))
+    #             job_data.append({
+    #                 'id': job.id,
+    #                 'name': job.name,
+    #                 'description': job.description,
+    #                 'department_id': job.department_id.name,
+    #                 'url': url
+    #                 # Add more fields as needed
+    #             })
+
+    #         # Convert list to JSON string
+    #         json_data = json.dumps(job_data)
+    #         return request.make_response(data=json_data, headers=[('Content-Type', 'application/json')])
+    #     except Exception as e:
+    #         se = _serialize_exception(e)
+    #         error = {
+    #             'code': 200,
+    #             'message': 'Something went wroung',
+    #             'data': se
+    #         }
+    #         return request.make_response(html_escape(json.dumps(error)))
