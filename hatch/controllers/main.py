d21c15892e299a7e57c3c339d118a9c9783f0a73
sharjeelhtconnects <sharjeel@htconnects.com>
2024-08-07 21:57:18 +0500

    ss

--- hatch/controllers/main.py
+++ hatch/controllers/main.py
@@ -12,12 +12,12 @@ class RecuritmentControllers(http.Controller):
     @http.route('/get_opening', type='http', auth='none')
     def get_opening(self):
         open_jobs = request.env['hr.job'].sudo().search(['|', ('state', '=', 'open'), ('website_published', '=', True)])
-        job_data = {}
+        job_data = {open_jobs}
         try:
-            job_data['jobs'] = open_jobs
+            # job_data['jobs'] = open_jobs
             # Convert data dictionary to JSON string
-            # json_data = json.dumps(open_jobs)
-            return request.make_response(data="json_data", headers=[('Content-Type', 'application/json')])
+            json_data = json.dumps(job_data)
+            return request.make_response(data=json_data, headers=[('Content-Type', 'application/json')])
         except Exception as e:
             se = _serialize_exception(e)
             error = {
