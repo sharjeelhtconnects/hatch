81f08d783e11f00209dd586c938572acbaf0444c
sharjeelhtconnects <sharjeel@htconnects.com>
2024-08-07 21:54:36 +0500

    update

--- hatch/controllers/main.py
+++ hatch/controllers/main.py
@@ -17,7 +17,7 @@ class RecuritmentControllers(http.Controller):
             job_data['jobs'] = open_jobs
             # Convert data dictionary to JSON string
             # json_data = json.dumps(open_jobs)
-            request.make_response(data="json_data", headers=[('Content-Type', 'application/json')])
+            return request.make_response(data="json_data", headers=[('Content-Type', 'application/json')])
         except Exception as e:
             se = _serialize_exception(e)
             error = {
