0255782b45e7103531dfccf8bcf41eb93abac622
sharjeelhtconnects <sharjeel@htconnects.com>
2024-08-09 16:05:47 +0500

    update

--- hatch/controllers/main.py
+++ hatch/controllers/main.py
@@ -55,6 +55,8 @@ class RecuritmentControllers(http.Controller):
                 job_data.append({
                     'id': job.id,
                     'title': job.name,
+                    'industry_id': job.industry_id,
+                    'industry_title': job.industry_id.name
                     # Add more fields as needed
                 })
 
